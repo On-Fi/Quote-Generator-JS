a6fa4cfd35045246fa4b70aa8b30c5f75d191b8e
onur fistikci <onurfistikci@mail.de>
2024-01-01 17:37:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const quotes = [
     "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
     "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
     "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
-    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt"
+    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+    "",
+    "",
+    "",
+    "",
+
 ];
 
 // The Function to generate a random quote
